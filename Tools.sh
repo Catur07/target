e4fdf381bc72b015729f5600635bd3820622ecef
MltrCyber <52839806+MltrCyber@users.noreply.github.com>
2019-10-14 18:51:41 +0700

    Add files via upload

---
+++ Tools.sh
@@ -0,0 +1,195 @@
+apt update && apt upgrade 
+pkg install git php curl nano python2
+pkg install lolcat toilet figlet
+clear
+echo
+toilet -f big -F gay "welcome"
+sleep 3
+figlet "LOADING..."|lolcat
+sleep 1
+echo "Mr.Cyber"|lolcat
+sleep 3
+echo "starting tools    ";
+sleep 0.1;
+clear
+echo "Starting tools.   ";
+sleep 0.1;
+clear
+echo "sTarting tools..  ";
+sleep 0.1;
+clear
+echo "stArting tools... ";
+sleep 0.1;
+clear
+echo "staRting tools    ";
+sleep 0.1;
+clear
+echo "starTing tools.   ";
+sleep 0.1;
+clear
+echo "startIng tools..  ";
+sleep 0.1;
+clear
+echo "startiNg tools... ";
+sleep 0.1;
+clear
+echo "startinG tools    ";
+sleep 0.1;
+clear
+echo "starting Tools.   ";
+sleep 0.1;
+clear
+echo "starting tOols..  ";
+sleep 0.1;
+clear
+echo "starting toOls... ";
+sleep 0.1;
+clear
+echo "starting tooLs    ";
+sleep 0.1;
+clear
+echo "starting toolS.   ";
+sleep 0.1;
+clear
+echo "starting tools..  ";
+sleep 0.1;
+clear
+echo "Starting tools... ";
+sleep 0.1;
+clear
+echo "sTarting tools    ";
+sleep 0.1;
+clear
+echo "stArting tools.   ";
+sleep 0.1;
+clear
+echo "staRting tools..  ";
+sleep 0.1;
+clear
+echo "starTing tools... ";
+sleep 0.1;
+clear
+echo "startIng tools    ";
+sleep 0.1;
+clear
+echo "startiNg tools.   ";
+sleep 0.1;
+clear
+echo "startinG tools..  ";
+sleep 0.1;
+clear
+echo "starting Tools... ";
+sleep 0.1;
+clear
+echo "starting tOols    ";
+sleep 0.1;
+clear
+echo "starting toOls.   ";
+sleep 0.1;
+clear
+echo "starting tooLs..  ";
+sleep 0.1;
+clear
+echo "starting toolS... ";
+sleep 0.1;
+clear
+echo "starting tools    ";
+sleep 0.1;
+clear
+echo "Starting tools.   ";
+sleep 0.1;
+clear
+echo "sTarting tools... ";
+sleep 0.1;
+clear
+echo "stArting tools    ";
+sleep 0.1;
+clear
+echo "staRting tools.   ";
+sleep 0.1;
+clear
+echo "starTing tools..  ";
+sleep 0.1;
+clear
+echo "startIng tools... ";
+sleep 0.1;
+clear
+echo "startiNg tools    ";
+sleep 0.1;
+clear
+echo "startinG tools.   ";
+sleep 0.1;
+clear
+echo "starting Tools..  ";
+sleep 0.1;
+clear
+echo "starting tOols... ";
+sleep 0.1;
+clear
+echo "starting toOls    ";
+sleep 0.1;
+clear
+echo "starting tooLs.   ";
+sleep 0.1;
+clear
+echo "starting toolS..  ";
+sleep 3;
+clear
+echo "     #########################################"|lolcat
+echo 
+echo "			Author    : Mr.Cyber"|lolcat
+echo "			My Team   : PERSEKUKHAYA T34M"|lolcat
+echo "			My Team   : Indonesia Cyber Army"|lolcat
+echo "			Thanks To : FRANS-O'CONER"|lolcat
+echo "     #########################################"|lolcat
+echo
+echo "       <<<<<<<<<<<<<<<<<=>>>>>>>>>>>>>>>>>"|lolcat
+echo "           [-] WELCOME TO MY TOOLS  [-]"|lolcat
+echo "       <<<<<<<<<<<<<<<<<=>>>>>>>>>>>>>>>>>"|lolcat
+echo "
+echo "                    ______________               "|lolcat                 
+echo "   		   ,===:'.,            `-._            "|lolcat                
+echo "                   `:.`---.__         `-._       "|lolcat                 
+echo "                     `:.     `--.         `.     "|lolcat                
+echo "                      \.        `.         `.    "|lolcat               
+echo "              (,,(,    \.         `.   ____,-`., "|lolcat               
+echo "           (,'     `/   \.   ,--.___`.'          "|lolcat               
+echo "       ,  ,'  ,--.  `,   \.;'         `          "|lolcat
+echo "        `{D, {    \  :    \;  FRANS-O,CONER      "|lolcat
+echo "          V,,'    /  /    //    Mr.Cyber         "|lolcat
+echo "          j;;    /  ,' ,-//.    ,---.      ,     "|lolcat
+echo "         \;'   /  ,' /  _  \  /  _  \   ,'/      "|lolcat
+echo "               \   `'  / \  `'  / \  `.' /       "|lolcat
+echo "                `.___,'   `.__,'   `.__,'		   "|lolcat
+echo "
+echo "
+echo "
+echo "
+echo "
+echo "
+echo "
+echo "
+echo "
+echo "
+echo "
+clear 
+echo "proses.";
+sleep 0.1
+echo "proses..";
+clear 
+sleep 0.2
+echo "proses...";
+clear 
+sleep 0.3
+echo "proses....";
+clear
+sleep 3
+echo "proses.....";
+echo "
+sleep 3
+git clone https://github.com/MltrCyber/MltrCyber123
+sleep3
+cd MltrCyber123
+sleep2
+sh 1.sh
+fi
